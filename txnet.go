09c05c5a092bb54e7ff4e16a353143e83bfdbfc1
wfunc <114468522+wfunc@users.noreply.github.com>
2024-12-21 12:18:40 +0800

    Update txnet.go

--- txnet.go
+++ txnet.go
@@ -357,11 +357,12 @@ func GetWagersSubDetailUrlBy3(wagersid, lang, username, gametype string) (resp x
 
 func yyyyMMDD() (yyyyMMDD string) {
 	now := time.Now()
-	loc, err := time.LoadLocation("America/New_York")
-	if err != nil {
-		fmt.Println("Error loading time zone:", err)
-		return
-	}
-	nyTime := now.In(loc)
+	// loc, err := time.LoadLocation("America/New_York")
+	// if err != nil {
+	// 	fmt.Println("Error loading time zone:", err)
+	// 	return
+	// }
+	// nyTime := now.In(loc)
+	nyTime := now.Add(-12 * time.Hour)
 	return nyTime.Format(`20060102`)
 }
